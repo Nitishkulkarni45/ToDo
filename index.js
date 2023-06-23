ec834aa8db2528a9d7d77bcf930a5f8d782c2cad
Nitishkulkarni45 <nitishdkulkarnisonu13122001@gmail.com>
2023-06-24 00:11:37 +0530

    need to update with timing

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const searchbox = document.getElementById("search-box");
 const listcont = document.getElementById("list-cont");
+const currentDate = new Date();
+let userInput;
+const date = new Date(userInput);
+
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: true
+};
+
+
 function addTask(){
     if(searchbox.value === ''){
         alert("You must write something!");
@@ -7,10 +22,18 @@ function addTask(){
     else{
        let li = document.createElement("li");
        li.innerHTML = searchbox.value;
+       userInput = prompt('Date/Time by which you would wish to complete your task?');
        listcont.appendChild(li);
+       
+      
        let span = document.createElement("span");
        span.innerHTML= "\u00d7";
        li.appendChild(span);
+       if(currentDate>=date){
+        alert("You have crossed the time limit set by you to finish the following task. Complete it asap!");
+        console.log("You have crossed!");
+       }
+       
     }
     searchbox.value = "";
     saveData()
@@ -40,3 +63,7 @@ function showList() {
   window.addEventListener("load", function() {
     showList();
   });
+    
+  const currentDateTime = currentDate.toLocaleString('en-US', options);
+  console.log(currentDateTime);
+//   alert(currentDateTime);
